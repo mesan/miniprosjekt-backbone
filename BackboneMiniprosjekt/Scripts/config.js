20740ed1711945cefb60310cffaab664d9c2962e
Øystein Skadsem <os@forskningsradet.no>
2012-12-04 16:18:24 +0100

    Lagt til en ekstra utkommentert mulig backend-url

--- BackboneMiniprosjekt/Scripts/config.js
+++ BackboneMiniprosjekt/Scripts/config.js
@@ -5,6 +5,7 @@ var app = app || {};
     "use strict";
     app.config = {
         apiBaseUrl: "http://searchparty.azurewebsites.net/api",
+//        apiBaseUrl: "http://searchpartyjava2.herokuapp.com",
         container: '#js-aksjoner-container',
         ruting: {
             // legg til mer ved behov
