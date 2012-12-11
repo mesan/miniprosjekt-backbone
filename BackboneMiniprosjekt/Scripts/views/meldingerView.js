30e30605dfb16046bb238a6de219a97961304a12
Lars Kristoffer Jørgensen <larsj@mesan.no>
2012-12-11 16:06:42 +0100

    Lagt til pushoppdatering av meldinger

--- BackboneMiniprosjekt/Scripts/views/meldingerView.js
+++ BackboneMiniprosjekt/Scripts/views/meldingerView.js
@@ -1,3 +1,4 @@
+
 /// <reference path="../_references.js" />
 var app = app || {};
 
@@ -8,8 +9,24 @@ var app = app || {};
         el: app.config.container,
         template: Handlebars.compile($("#meldinger-template").html()),
         render: function () {
+            this.$el.empty();
             this.$el.html(this.template(this.collection.toJSON()));
             return this;
+        },
+        initialize: function () {
+            _.bind(this.bindPushUpdate, this)();
+        },
+
+        bindPushUpdate: function () {
+            var self = this,
+                aksjonId = 1,
+                pusher = new Pusher('76b8bc80e33cffd17524'),
+                channel = pusher.subscribe('leteaksjon-' + aksjonId);
+
+            channel.bind('ny-melding', function (data) {
+                self.collection.add(JSON.parse(data));
+                self.render();
+            });
         }
     });
 
