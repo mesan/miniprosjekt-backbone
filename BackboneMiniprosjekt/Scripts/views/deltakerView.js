81d3c4d03be52903e14f4d33648b7e9e0c619087
Bendik Rostad <bendikr@mesan.no>
2013-01-08 17:12:15 +0100

    Lagt til mulighet for å sette deltaker aktiv eller ikke (Så langt backend støtter det)

--- BackboneMiniprosjekt/Scripts/views/deltakerView.js
+++ BackboneMiniprosjekt/Scripts/views/deltakerView.js
@@ -7,19 +7,21 @@ var app = app || {};
 	app.DeltakerView = Backbone.View.extend({
 		events : {
 			"keypress .mobilnummer-input" : "updateOnEnter",
-			"click .close" : "deleteSelf"
+			"click .close" : "deleteSelf",
+			"click .deltaker-check" : "updateModel"
 		},
 		render : function() {
 			var template = Handlebars.compile($("#deltaker-template").html());
 			this.$el.html(template({
 				mobilnummer : this.model.get('mobilnummer'),
-				id : this.model.id
+				id : this.model.id,
+				aktiv : this.model.get("aktiv")
 			}));
 			return this;
 		},
 		
 		updateAttributes : function() {
-			alert("Ikke støttet av backend");
+			alert("Oppdatering av model er ikke støttet av backend...");
 			//Not yet supported by the API
 //			this.model.save({
 //				'mobilnummer' : this.$el.find('.mobilnummer-input').val()
@@ -31,6 +33,10 @@ var app = app || {};
 			if (e.keyCode == 13) {
 				this.updateAttributes();				
 			}
+		},
+		
+		updateModel : function(e) {
+			this.updateAttributes();
 		},
 		
 		//Deletes the model and removes the view
